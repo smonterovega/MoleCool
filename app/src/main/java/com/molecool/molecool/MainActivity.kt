aeda71889b524d3afb589faa459676ff5ecb6c55
dilesh <drj855@mun.ca>
2020-05-28 23:17:03 -0230

    app landing page and game page. navigation graph

--- app/src/main/java/com/molecool/molecool/MainActivity.kt
+++ app/src/main/java/com/molecool/molecool/MainActivity.kt
@@ -1,13 +1,11 @@
 package com.molecool.molecool
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.content_main.*
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
